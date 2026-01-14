923e07a1a551a93dada76fc4fe0c331ba48aaf3b
rhuel01 <rhuelalterdado@gmail.com>
2026-01-14 10:57:30 +0800

    Update main.dart

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screen/get_started_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -15,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF4E3629), // Deep Mocha
+          seedColor: const Color(0xFF4E3629),
           primary: const Color(0xFF4E3629),
           secondary: const Color(0xFF8D6E63),
         ),
